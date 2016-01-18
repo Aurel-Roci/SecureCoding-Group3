63749e7d6c3cd519da0987409748896978a6c6d0
Aurel-Roci <aurelroci@hotmail.com>
2016-01-18 13:09:02 +0100

    fixes

--- online_banking/applicationDownload.php
+++ online_banking/applicationDownload.php
@@ -1,5 +1,6 @@
 <?php
-$user_id = $_GET['user_id'];
+$user_id = intval($_GET['user_id']);
+$user_id = escapeshellarg($user_id);
 header('Content-type: application/octet-stream');
 header('Content-Disposition: attachment; filename="scs.jar"');
 readfile("/tmp/". $user_id ."/scs.jar", "r");
